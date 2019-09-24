577606f8f2558e71e9d7e8e3e2c1f4f2d3cb895a
zohzik <theynel3@gmail.com>
2019-09-24 09:16:12 -0500

    added models for products and wishlists

--- SwagShopAPI/server.js
+++ SwagShopAPI/server.js
@@ -4,9 +4,12 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var db = mongoose.connect('mongodb://localhost/swag-shop', {useNewUrlParser: true, useUnifiedTopology: true});
 
+var Product = require("./models/product");
+var WishList = require("./models/wishList");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(3000, function(){
   console.log("Swag shop api running on port 3000");
-})
+});
