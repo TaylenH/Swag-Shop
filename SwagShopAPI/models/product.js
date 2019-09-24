fcd81eee0e4dd4ef48e4a350cc3e4fbf2c0b4080
zohzik <theynel3@gmail.com>
2019-09-24 09:29:33 -0500

    added create endpoint

--- SwagShopAPI/models/product.js
+++ SwagShopAPI/models/product.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 var product = new Schema({
   title: String,
   price: Number,
-  likes: Number
+  likes: {
+    type: Number,
+    default: 0
+  }
 });
 
 module.exports = mongoose.model('Product', product);
--- SwagShopAPI/server.js
+++ SwagShopAPI/server.js
@@ -2,14 +2,34 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var db = mongoose.connect('mongodb://localhost/swag-shop', {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connect('mongodb://localhost/swag-shop', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 var Product = require("./models/product");
 var WishList = require("./models/wishList");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.listen(3000, function(){
+app.post('/product', function(req, res) {
+  var product = new Product();
+  product.title = req.body.title;
+  product.price = req.body.price;
+  product.save(function(err, savedProduct) {
+    if (err) {
+      res.status(500).send({
+        error: "Could not save product"
+      });
+    } else {
+      res.send(savedProduct);
+    }
+  });
+});
+
+app.listen(3000, function() {
   console.log("Swag shop api running on port 3000");
 });
